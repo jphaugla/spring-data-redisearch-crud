3844e702dd31fb06e225f8c0692f430aa18b770c
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2020-02-13 12:18:02 -0600

    add redis config

---
+++ src/main/java/com/jphaugla/config/RedisConfig.java
@@ -0,0 +1,19 @@
+package com.jphaugla.config;
+
+import com.jphaugla.domain.ProductEntity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import com.rnbwarden.redisearch.client.RediSearchClient;
+
+
+@DependsOn("RediSearchAutoConfiguration")
+@Configuration
+@ComponentScan("com.jphaugla")
+
+public class RedisConfig {
+    RediSearchClient<ProductEntity> rediSearchClient;
+    }
+
+}
