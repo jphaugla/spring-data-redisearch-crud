617b7f259f47ea30747b86ef5a4e5ce6ac6f36e2
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2020-03-23 12:33:13 -0500

    make connections configurable

--- src/main/java/com/jphaugla/config/RedisConfig.java
+++ src/main/java/com/jphaugla/config/RedisConfig.java
@@ -1,17 +1,39 @@
 package com.jphaugla.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jphaugla.domain.ProductEntity;
+import com.rnbwarden.redisearch.CompressingJacksonSerializer;
+import com.rnbwarden.redisearch.client.jedis.JedisRediSearchClient;
+import io.redisearch.client.Client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import com.rnbwarden.redisearch.client.RediSearchClient;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 
-@DependsOn("RediSearchAutoConfiguration")
+// @DependsOn("RediSearchAutoConfiguration")
 @Configuration
 @ComponentScan("com.jphaugla")
 
 public class RedisConfig {
-    RediSearchClient<ProductEntity> rediSearchClient;
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("server", 6379);
+        return new JedisConnectionFactory(config);
+    }
+    @Bean
+    public JedisRediSearchClient createJedisRediSearchClient() {
+
+        Class<ProductEntity> clazz = ProductEntity.class;
+        RedisSerializer<ProductEntity> redisSerializer = new CompressingJacksonSerializer<>(clazz, new ObjectMapper());
+        Client rediSearchClient = new Client("stub", "localhost", 6379);
+        return new JedisRediSearchClient<>(clazz, rediSearchClient, redisSerializer, 1000L);
+    }
+
+
 }
