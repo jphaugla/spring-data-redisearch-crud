b063ff29769eab32980b58ea21071c98236e0b0e
Jason Haugland <jasonhaugland@Jasons-MacBook-Pro-2.local>
2020-03-23 13:27:58 -0500

    add more products using scripts

--- scripts/postProduct.sh
+++ scripts/postProduct.sh
@@ -1,1 +1,2 @@
+#  add on ADIDAS shoe
 curl -X PUT -H "Content-Type: application/json" -d '{ "id": "z123", "articleNumber": "Superstar", "brand": "ADIDAS", "skus": [ { "key":"a01", "attributes": {"color": "black", "price": "99.99"}}, {"key":"a02",  "attributes":{"color": "white", "price": "99.99"}} ]}' http://localhost:8080/put_ProductEntity
